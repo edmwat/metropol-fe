f83d3cb00d8abc11c2d3dc4cc7bd67fe472346a8
edmwat <edmwat@gmail.com>
2022-03-28 08:59:54 +0300

    refined account template

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, RouterEvent } from '@angular/router';
 import { Account } from '../models/account';
 import { AccountsService } from '../services/accounts.service';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-accounts',
@@ -11,10 +12,14 @@ import { AccountsService } from '../services/accounts.service';
 })
 export class AccountsComponent implements OnInit {
   formGroup!: FormGroup;
+  loggedinUser:string="";
 
-  constructor(private _formBuilder:FormBuilder, private accountService:AccountsService,private router:Router) { }
+  constructor(private _formBuilder:FormBuilder, private accountService:AccountsService,private router:Router,private authenticationService:AuthenticationService) { }
 
   ngOnInit(): void {
+    this.authenticationService.getLoggedinUser().subscribe(res=>{
+      this.loggedinUser = res.username;
+    })
     this.formGroup = this._formBuilder.group({
       id: ['', Validators.email],
       iban: ['',Validators.required],
@@ -29,8 +34,9 @@ export class AccountsComponent implements OnInit {
     account.id = this.formGroup.controls['id'].value
     account.iban = this.formGroup.controls['iban'].value
     account.bicSwift = this.formGroup.controls['bicSwift'].value
-    account.clientId = this.formGroup.controls['clientId'].value
+    account.clientId = this.loggedinUser; //this.formGroup.controls['clientId'].value
 
+console.log(account)
     this.accountService.addNewAccount(account);
     this.router.navigate(['dashboard']);
 
